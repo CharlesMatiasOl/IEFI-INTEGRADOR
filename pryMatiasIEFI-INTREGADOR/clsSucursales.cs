5199908d779b26ac376b9fa11b36215b06155c5c
Charles Matias <Charlesmatias39@gmail.com>
2022-11-15 16:58:51 -0300

    todo

--- pryMatiasIEFI-INTREGADOR/clsSucursales.cs
+++ pryMatiasIEFI-INTREGADOR/clsSucursales.cs
@@ -11,8 +11,69 @@ namespace pryMatiasIEFI_INTREGADOR
 {
     internal class clsSucursales
     {
-        
-        
+        private OleDbConnection conexion = new OleDbConnection();
+        private OleDbCommand comando = new OleDbCommand();//enviamos ordenes a las bases de dapto
+        private OleDbDataAdapter adaptador = new OleDbDataAdapter();//adpatamos los datos que estan en la base a datos comprensibles por .NET
+
+        private string cadenaConexion = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=IEFIBD.mdb";
+        private string tabla = "Sucursales";
+
+        public void ListarComboBox(ComboBox combo)
+        {
+            try
+            {
+                conexion.ConnectionString = cadenaConexion;//cadena de conexion
+                conexion.Open();
+                comando.Connection = conexion; //el comando necesita tener una conexion
+                comando.CommandType = CommandType.TableDirect; //nos trae una tabla
+                comando.CommandText = tabla;
+                adaptador = new OleDbDataAdapter(comando);
+                DataSet DS = new DataSet();//tabla en memoria ram que tiene datos de mi tabla
+                adaptador.Fill(DS, tabla);
+                combo.DataSource = DS.Tables[tabla];
+                combo.DisplayMember = "Detalle_Sucursal";//es lo que va aparecer en la lista desplegable
+                combo.ValueMember = "Codigo_Sucursal";//esta invisible, pero esta
+
+
+                conexion.Close();
+
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+
+        }
+        public void FiltrarClientesDeUnaSucursal(DataGridView Grilla, Int32 Sucursal)
+        {
+            try
+            {
+
+                conexion.ConnectionString = cadenaConexion;
+                conexion.Open();
+                comando.Connection = conexion;
+                comando.CommandType = CommandType.TableDirect;
+                comando.CommandText = "Socio";
+                OleDbDataReader Lector = comando.ExecuteReader();
+                Grilla.Rows.Clear();
+                if (Lector.HasRows)// si hay filas se lee
+                {
+                    while (Lector.Read())//leemos
+                    {
+                        if (Lector.GetInt32(3) == Sucursal)
+                        {
+                            Grilla.Rows.Add(Lector.GetInt32(0), Lector.GetString(1), Lector.GetString(2), Lector.GetDecimal(5));
+                        }
+                    }
+                }
+                conexion.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
 
     }
 }
